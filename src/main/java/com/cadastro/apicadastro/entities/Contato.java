7a85aee889a33980c83e464df145f25570f5902b
lucaschianca-dev <lucaseduardochianca.dev@gmail.com>
2023-10-27 12:41:14 -0300

    criado entidade 'Contato'. mapeamento objeto relacional em andamento

---
+++ src/main/java/com/cadastro/apicadastro/entities/Contato.java
@@ -0,0 +1,24 @@
+package com.cadastro.apicadastro.entities;
+
+import com.cadastro.apicadastro.enums.TipoContato;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "contatos")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Contato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.ORDINAL)
+    private TipoContato tipoContato;
+
+    private String contato;
+}
