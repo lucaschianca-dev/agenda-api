443c6e4a717af733315689f4a4d0a77968aa09ac
lucaschianca-dev <lucaseduardochianca.dev@gmail.com>
2023-10-27 12:39:24 -0300

    codigo refatorado

--- src/main/java/com/cadastro/apicadastro/ApiCadastroApplication.java
+++ src/main/java/com/cadastro/apicadastro/ApiCadastroApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiAgendaApplication {
+public class ApiCadastroApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiAgendaApplication.class, args);
+		SpringApplication.run(ApiCadastroApplication.class, args);
 	}
 
 }
