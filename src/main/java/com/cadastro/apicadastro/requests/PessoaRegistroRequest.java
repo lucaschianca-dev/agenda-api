ed93669482c26818d828b9d265ffd54951543c42
lucaschianca-dev <lucaseduardochianca.dev@gmail.com>
2023-10-27 16:00:01 -0300

    criado o request do registro da entidade 'Pessoa'

---
+++ src/main/java/com/cadastro/apicadastro/requests/PessoaRegistroRequest.java
@@ -0,0 +1,32 @@
+package com.cadastro.apicadastro.requests;
+
+import com.cadastro.apicadastro.entities.Contato;
+import com.cadastro.apicadastro.entities.Endereco;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PessoaRegistroRequest {
+
+    private String nome;
+
+    private LocalDate idade;
+
+    private String cpf;
+
+    private String genero;
+
+    private String estadoCivil;
+
+    private String nacionalidade;
+
+    private Endereco endereco;
+
+    private List<Contato> contatos;
+}
