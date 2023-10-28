2c7384e7eca20b5c0780ab1e031a63e1f9623636
lucaschianca-dev <lucaseduardochianca.dev@gmail.com>
2023-10-28 15:44:28 -0300

    refatorado metodo para registrar pessoa no service

--- src/main/java/com/cadastro/apicadastro/services/PessoaService.java
+++ src/main/java/com/cadastro/apicadastro/services/PessoaService.java
@@ -1,27 +1,39 @@
 package com.cadastro.apicadastro.services;
 
 import com.cadastro.apicadastro.dtos.PessoaDTO;
+import com.cadastro.apicadastro.entities.Contato;
 import com.cadastro.apicadastro.entities.Pessoa;
 import com.cadastro.apicadastro.mapper.PessoaMapper;
 import com.cadastro.apicadastro.repositories.PessoaRepository;
 import com.cadastro.apicadastro.requests.PessoaRegistroRequest;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class PessoaService {
 
     private PessoaRepository pessoaRepository;
 
-    public PessoaService(PessoaRepository pessoaRepository) {
-        this.pessoaRepository = pessoaRepository;
-    }
+    private static List<Contato> contatos = new ArrayList<>();
 
     @Transactional
     public PessoaDTO registraPessoa(@Valid PessoaRegistroRequest request) {
-        Pessoa pessoa = pessoaRepository.save(PessoaMapper.INSTANCE.toPessoa(request));
+        Pessoa pessoa = PessoaMapper.INSTANCE.toPessoa(request);
 
-        return PessoaMapper.INSTANCE.toPessoaDTO(pessoa);
+        for(Contato contato: request.getContatos()){
+            contato.setPessoa(pessoa);
+            contatos.add(contato);
+        }
+        pessoa.setContatos(contatos);
+
+        Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+
+        return PessoaMapper.INSTANCE.toPessoaDTO(pessoaSalva);
     }
 }
