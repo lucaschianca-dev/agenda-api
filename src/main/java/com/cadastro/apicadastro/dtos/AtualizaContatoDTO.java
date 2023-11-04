86be4179561974e6cf605084dbe47db27cac3f7c
lucaschianca-dev <lucaseduardochianca.dev@gmail.com>
2023-11-04 17:12:09 -0300

    refatorado validacao para atualizar contato

--- src/main/java/com/cadastro/apicadastro/dtos/AtualizaContatoDTO.java
+++ src/main/java/com/cadastro/apicadastro/dtos/AtualizaContatoDTO.java
@@ -1,17 +1,16 @@
 package com.cadastro.apicadastro.dtos;
 
 import com.cadastro.apicadastro.enums.TipoContato;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record AtualizaContatoDTO(
 
-        @NotBlank(message = "Nome é obrigatório")
-        @Pattern(regexp = ".{2,50}", message = "O nome precisa ter entre 2 e 50 caracteres")
+        @Pattern(regexp = ".{2,30}", message = "O nome precisa ter entre 2 e 30 caracteres")
         String nome,
 
         TipoContato tipoContato,
 
+        @Pattern(regexp = ".{2,50}", message = "O contato precisa ter entre 2 e 50 caracteres")
         String contato
 ) {
 }
